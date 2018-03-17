dedab6d3dbde1b17bb157b2ab1d78774cd46a8b0
N Fourtounis <nickfortune@windowslive.com>
2018-03-17 19:27:48 +0200

    Initial commit

---
+++ src/pythonpractice.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "NFourtounis"
+__date__ = "$Mar 17, 2018 7:26:32 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
