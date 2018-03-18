0bf56759e4c4b1f443e38291b04e72c241e85022
N Fourtounis <nickfortune@windowslive.com>
2018-03-18 17:19:58 +0200

    comments

--- src/pythonpractice.py
+++ src/pythonpractice.py
@@ -6,11 +6,16 @@ __author__ = "NFourtounis"
 __date__ = "$Mar 17, 2018 7:26:32 PM$"
 
 if __name__ == "__main__":
+    #Print
+    '''
     a = "Hello"
     b = "World"
     a+b
-    #var = input("Whats's your name?")
-    #print(a,var)
+    var = input("Whats's your name?")
+    print(a,var)'''
+    
+    #Numerical
+    '''
     print(3+2, 8 ** 2, 19/5, 19//5, 19%5)
     print(3+2, 8 ** 2, 19/5, 19//5, 19%5, sep='#')
     abs(-5)
@@ -20,3 +25,11 @@ if __name__ == "__main__":
     x = 5
     x += 1
     x **= 2
+    print(x)'''
+    
+    #Strings
+    str1 = "Fish & Chips"
+    str1[7:11] 
+    str1[11:7] 
+    print(str1)
+    
