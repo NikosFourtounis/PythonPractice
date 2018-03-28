74826c6b2511cdb1738f3b380fc35d9b44b8ebe1
N Fourtounis <nickfortune@windowslive.com>
2018-03-28 20:25:50 +0300

    dictionaries

--- src/pythonpractice2.py
+++ src/pythonpractice2.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     print (x, y)
     '''
     #tuples
+    '''
     places = [("Karlovasi", 9000), ("Vathy", 11000), ("Pythagorio", 8000)]
     print("Population of {0} is {1}".format(places[2][0], places[2][1]))
     list1 = [1, 4, 7, 9]
@@ -20,4 +21,14 @@ if __name__ == "__main__":
     print(list1)
     places.sort(reverse=True)
     print(places)
+    '''
+    #dictionaries
+    my_dict={1:'one'}
+    print(my_dict[1])
+    my_dict[2]='two'
+    print(my_dict)
+    print(2 in my_dict) # True αν υπάρχει το 2 στα κλειδιά (keys)
+    list1 = [(1,"One"), (2,"Two"), (3,"Three")]
+    print(dict(list1))
+
 
