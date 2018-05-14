20aa83d781be687a73692f4f23a1bf4b3177cd81
N Fourtounis <nickfortune@windowslive.com>
2018-05-14 19:49:56 +0300

    Pet class

---
+++ src/pythonpractice4.py
@@ -0,0 +1,21 @@
+__author__ = "NFourtounis"
+__date__ = "$May 14, 2018 7:42:00 PM$"
+from datetime import datetime
+class Pet(object):
+    def __init__(self,name,breed,area):
+        self.name=name
+        self.breed=breed
+        self.area=area
+        print('Created',self.name,', ',self.breed,', ',self.area)
+    def getName(self):
+        return self.name
+    def getBreed(self):
+        return self.breed
+    def getArea(self):
+        return self.area
+if __name__ == "__main__":
+    a1 = Pet('Doge','Doggoo','Greece')
+    print(a1.getName())
+    print(a1.getBreed())
+    print(a1.getArea())
+    
